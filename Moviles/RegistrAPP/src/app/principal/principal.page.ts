502fbcd393492c54220fe71f3f1c0f5fb6059cc5
CristianUbilla <89488061+CristianUbilla@users.noreply.github.com>
2022-09-08 14:17:44 -0300

    Detalle corregido

--- Moviles/RegistrAPP/src/app/principal/principal.page.ts
+++ Moviles/RegistrAPP/src/app/principal/principal.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 /*import { NavController } from 'ionic-angular';*/
 /*import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';*/
 @Component({
