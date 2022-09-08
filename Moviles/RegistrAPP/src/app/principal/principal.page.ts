47fb0ff55c10f4e4d532a306ceeddf0cafe891aa
Cristian <cri.ubilla@duocuc.cl>
2022-09-08 04:19:46 -0300

    Mejor diseño

--- Moviles/RegistrAPP/src/app/principal/principal.page.ts
+++ Moviles/RegistrAPP/src/app/principal/principal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+/*import { NavController } from 'ionic-angular';*/
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  /*constructor(public navCtrl: NavController, private camera: Camera)*/
+
+  /*takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }*/
 
   ngOnInit() {
   }
